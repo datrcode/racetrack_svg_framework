Fixed masking issue; experimented with distributions.

framework/rt_text_mixin.py
@@ -1112,10 +1112,13 @@ class RTTextMixin(object):
             _token                  = tokenizer.decode(_inputs['input_ids'][0][token_i])
             i0 = sentence.index(_token, i0)
             _token_stripped_lowered = _token.strip().lower()
-            _before = ' ' if _token[0]  == ' ' else ''
-            _after  = ' ' if _token[-1] == ' ' else ''
-            _sentence_w_mask = tokenizer.decode(_inputs['input_ids'][0][1:token_i]) + _before + '<mask>' + _after + tokenizer.decode(_inputs['input_ids'][0][token_i+1:-1])
-            _inputs_w_mask   = tokenizer(_sentence_w_mask, return_tensors="pt").to(device)
+            #_before = ' ' if _token[0]  == ' ' else ''
+            #_after  = ' ' if _token[-1] == ' ' else ''
+            #_sentence_w_mask = tokenizer.decode(_inputs['input_ids'][0][1:token_i]) + _before + '<mask>' + _after + tokenizer.decode(_inputs['input_ids'][0][token_i+1:-1])
+            #_inputs_w_mask   = tokenize(_sentence_w_mask).to(device)
+            _inputs_w_mask                          = tokenizer(sentence, return_tensors="pt")
+            _inputs_w_mask['input_ids'][0][token_i] = tokenizer.encode('<mask>')[1]
+            _inputs_w_mask.to(device)
             with torch.no_grad():
                 _output = model(**_inputs_w_mask)
                 _logits = _output.logits