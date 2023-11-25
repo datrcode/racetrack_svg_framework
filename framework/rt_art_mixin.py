52c9a83eaf15d9b1955bd7f8f27271a55ce75d2e
D.T <dave.trimm@gmail.com>
2023-11-25 14:21:36 -0500

    More icon artwork.

--- framework/rt_art_mixin.py
+++ framework/rt_art_mixin.py
@@ -47,6 +47,85 @@ class RTArtMixin(object):
                f'</svg>'
 
     #
+    # iconHistogram()
+    #
+    def iconHistogram(self, x=0, y=0, w=200, h=200, fill='#a0a0a0', fg='#000000', bg='none', stroke_width=3, x_ins=10, y_ins=10):
+         return f'<svg x="{x}" y="{y}" width="{w}" height="{h}" viewbox="{-x_ins} {-y_ins} {100+x_ins} {100+y_ins}" xmlns="http://www.w3.org/2000/svg">' + \
+                f'<path d="M 10 0 L  90   0  C   95   0   100   5   100   10' + \
+                f'                L 100  90  C  100  95    95 100    90  100' + \
+                f'                L  10 100  C    5 100     0  95     0   90' + \
+                f'                L  0   10  C    0   5     5   0    10    0" fill="{bg}" stroke="{fg}" stroke-width="{stroke_width}" />' + \
+                f'<rect x="15" y="5"  width="70" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="15" width="65" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="25" width="55" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="35" width="30" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="45" width="30" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="55" width="20" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="65" width="15" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="75" width="10" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'<rect x="15" y="85" width="10" height="10" stroke="{fg}" fill="{fill}" />' + \
+                f'</svg>'
+
+    #
+    # iconTemporalBarChart()
+    #
+    def iconTemporalBarChart(self, x=0, y=0, w=200, h=200, fill='#a0a0a0', fg='#000000', bg='none', stroke_width=3, x_ins=10, y_ins=10):
+        return f'<svg x="{x}" y="{y}" width="{w}" height="{h}" viewbox="{-x_ins} {-y_ins} {100+x_ins} {100+y_ins}" xmlns="http://www.w3.org/2000/svg">' + \
+               f'<path d="M 10 0 L  90   0  C   95   0   100   5   100   10' + \
+               f'                L 100  90  C  100  95    95 100    90  100' + \
+               f'                L  10 100  C    5 100     0  95     0   90' + \
+               f'                L  0   10  C    0   5     5   0    10    0" fill="{bg}" stroke="{fg}" stroke-width="{stroke_width}" />' + \
+               f'<rect x="12" y="50"  width="10" height="30" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="22" y="45"  width="10" height="35" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="32" y="40"  width="10" height="40" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="42" y="45"  width="10" height="35" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="52" y="50"  width="10" height="30" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="62" y="55"  width="10" height="25" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="72" y="60"  width="10" height="20" stroke="{fg}" fill="{fill}" />' + \
+               f'<path d="M 10 80 L 90 80 L 80 85" fill="none" stroke="{fg}"/>' + \
+               f'<path d="M 10 80 L 10 10 L 5 20"  fill="none" stroke="{fg}"/>' + \
+               f'<text x="50" y="95" font-family="times" font-size="12" text-anchor="middle">Time</text>' + \
+               f'</svg>'
+
+    #
+    # iconPeriodicBarChart()
+    #
+    def iconPeriodicBarChart(self, x=0, y=0, w=200, h=200, fill='#a0a0a0', fg='#000000', bg='none', stroke_width=3, x_ins=10, y_ins=10):
+        return f'<svg x="{x}" y="{y}" width="{w}" height="{h}" viewbox="{-x_ins} {-y_ins} {100+x_ins} {100+y_ins}" xmlns="http://www.w3.org/2000/svg">' + \
+               f'<path d="M 10 0 L  90   0  C   95   0   100   5   100   10' + \
+               f'         L 100  90  C  100  95    95 100    90  100' + \
+               f'         L  10 100  C    5 100     0  95     0   90' + \
+               f'         L  0   10  C    0   5     5   0    10    0" fill="{bg}" stroke="{fg}" stroke-width="{stroke_width}" />' + \
+               f'<rect x="12" y="50"  width="10" height="30" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="22" y="45"  width="10" height="35" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="32" y="40"  width="10" height="40" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="42" y="45"  width="10" height="35" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="52" y="50"  width="10" height="30" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="62" y="55"  width="10" height="25" stroke="{fg}" fill="{fill}" />' + \
+               f'<rect x="72" y="60"  width="10" height="20" stroke="{fg}" fill="{fill}" />' + \
+               f'<path d="M 10 80 L 85 80" fill="{bg}" stroke="{fg}"/>' + \
+               f'<path d="M 10 80 L 10 30 L 5 40" fill="{bg}" stroke="{fg}"/>' + \
+               f'<path d="M 70 50 A 20 20 0 0 0 30 50 M 70 50 A 20 20 0 0 1 30 50" fill="{bg}" stroke="{fg}" stroke-width="{stroke_width+5}"/>' + \
+               f'<path d="M 58 30 l 20 5 l -20 15 Z" fill="{fg}"/>' + \
+               f'<text x="50" y="95" font-family="times" font-size="12" text-anchor="middle">Time</text>' + \
+               f'</svg>'
+
+
+    #
+    # iconPieChart()
+    #
+    def iconPieChart(self, x=0, y=0, w=200, h=200, fill_a='#a0a0a0', fill_b='#404040', fg='#000000', bg='none', stroke_width=3, x_ins=10, y_ins=10):
+        return f'<svg x="{x}" y="{y}" width="{w}" height="{h}" viewbox="{-x_ins} {-y_ins} {100+x_ins} {100+y_ins}" xmlns="http://www.w3.org/2000/svg">' + \
+               f'<path d="M 10 0 L  90   0  C   95   0   100   5   100   10' + \
+               f'         L 100  90  C  100  95    95 100    90  100' + \
+               f'         L  10 100  C    5 100     0  95     0   90' + \
+               f'         L  0   10  C    0   5     5   0    10    0" fill="{bg}" stroke="{fg}" stroke-width="{stroke_width}" />' + \
+               f'<path d="M 90 50 A 20 20 0 0 0 10 50 M 90 50 A 20 20 0 0 1 10 50" fill="{bg}" stroke="{fg}" stroke-width="{stroke_width+2}"/>' + \
+               f'<path d="M 50 50 l 0  -40 a 40 40 0 0 10 80 Z" fill="{fill_a}" stroke="{fg}" stroke-width="{stroke_width}"/>' + \
+               f'<path d="M 50 50 l 35 -20 a 40 40 0 0 10 40 Z" fill="{fill_b}" stroke="{fg}" stroke-width="{stroke_width}"/>' + \
+               f'</svg>'
+
+    #
     # iconSetCurrentAsRoot()
     #
     def iconSetCurrentAsRoot(self, x=0, y=0, w=200, h=200, node_fill='#808080', fg='#000000', bg='none', arrow_stroke_width=5, stroke_width=3, x_ins=10, y_ins=10):
