2a1d10e2c19d7b84407664e5ad7012a9bfe05309
DT <dave.trimm@gmail.com>
2024-06-13 19:05:01 -0400

    Counting options prototyped... but issues with render.

--- framework/rt_link_mixin.py
+++ framework/rt_link_mixin.py
@@ -583,7 +583,15 @@ class RTLinkMixin(object):
                 self.linkcols.append(_link_)
 
             # Uniquify the x0,y0 -> x1,y1 coordinates ... then format it into svg lines
-            self.df_link = self.df.group_by(_gb_str_).agg(pl.len().alias('__count__')).with_columns(*_operations_)
+            # count by a field
+            if self.link_size == 'vary' and self.count_by_set == False and self.count_by in self.df:
+                self.df_link = self.df.group_by(_gb_str_).agg(pl.sum(self.count_by).alias('__count__')).with_columns(*_operations_)
+            # count by a field (set-based)
+            elif self.link_size == 'vary' and self.count_by_set == False and self.count_by in self.df:
+                self.df_link = self.df.group_by(_gb_str_).agg(pl.col(self.count_by).n_unique().alias('__count__')).with_columns(*_operations_)
+            # else either no counting or by the number of rows
+            else:
+                self.df_link = self.df.group_by(_gb_str_).agg(pl.len().alias('__count__')).with_columns(*_operations_)
 
             # Return the list of links
             _set_ = set()
