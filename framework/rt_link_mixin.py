f5b52c25976e7e1d1c23f93a93fac58808e3b1a4
DT <dave.trimm@gmail.com>
2024-06-20 19:20:22 -0400

    Fixed svg less than and greater than symbol issues.

--- framework/rt_link_mixin.py
+++ framework/rt_link_mixin.py
@@ -701,6 +701,7 @@ class RTLinkMixin(object):
                 if self.label_only  is not None and len(self.label_only)  > 0: df_node_labels = self.df_node.filter(pl.col('__first__').is_in(self.label_only)) # Filter
                 df_node_labels = df_node_labels.with_columns(pl.col('__first__').cast(pl.Utf8).alias('__label__'))
                 if self.node_labels is not None and len(self.node_labels) > 0: df_node_labels = df_node_labels.with_columns(pl.col('__first__').replace(self.node_labels).alias('__label__'))
+                df_node_labels = df_node_labels.with_columns(pl.col('__label__').str.replace_all('&','&amp').str.replace_all('<','&lt;').str.replace_all('>','&gt;'))
                 _str_op_ = [pl.lit('<text x="'), pl.col('__sx__'), pl.lit('" y="'), pl.col('__sy__') + pl.col('__sz__') + self.txt_h,
                             pl.lit(f'" font-size="{self.txt_h}px" text-anchor="middle">'), pl.col('__label__'),
                             pl.lit('</text>')]
