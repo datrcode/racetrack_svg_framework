697ef0ccfe3331d896147a1caad328d69975d70d
DT <dave.trimm@gmail.com>
2024-06-26 18:28:55 -0400

    Restarts can now happen with partially processed files.

--- use_cases/2014_vast_challenge/structure_articles_phi.py
+++ use_cases/2014_vast_challenge/structure_articles_phi.py
@@ -45,21 +45,33 @@ def promptModel(prompt, max_tokens=500, temperature=0.0, do_sample=False):
     return output[0]['generated_text']
 
 import os
-_base_dir_ = '../../../data/2014_vast/MC1/News Articles'
-
+from os.path import exists
 import pandas as pd
 import numpy as np
 import time
 
+_lu_       = {'file':[], 'article':[], 'phi_response':[], 'phi_time':[]}
+
+done = set()
+if exists('phi_2014_vast.csv.partial'):
+    _df_ = pd.read_csv('phi_2014_vast.csv.partial')
+    _lu_['file'].extend(_df_['file'])
+    _lu_['article'].extend(_df_['article'])
+    _lu_['phi_response'].extend(_df_['phi_response'])
+    _lu_['phi_time'].extend(_df_['phi_time'])
+    done = set(_df_['article'])
+
+_base_dir_ = '../../../data/2014_vast/MC1/News Articles'
+
 ts0 = time.time()
 files_processed = 0
 
-_lu_       = {'file':[], 'article':[], 'phi_response':[], 'phi_time':[]}
 _prompt_   = 'Parse the following text into an ontology of people, places, things, and events.  Extract the relationships betwen entities.  Return just the JSON structure.'
 for _dir_ in os.listdir(_base_dir_):
     for _file_ in os.listdir(os.path.join(_base_dir_, _dir_)):
         _article_raw_ = open(os.path.join(_base_dir_, _dir_, _file_), 'rb').read()
         _article_     = str(_article_raw_) #.replace('\\r', '').split('\\n')
+        if _article_ in done: continue
         ts0_model = time.time()
         _response_    = promptModel(_prompt_ + '\n\n' + _article_, max_tokens=4096)
         ts1_model = time.time()
