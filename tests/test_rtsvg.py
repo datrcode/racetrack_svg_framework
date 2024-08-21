141fa485b3adb45441dd679ee88b176560a987d5
D Trimm <dave.trimm@gmail.com>
2024-08-21 17:20:46 -0400

    Conforms to unit testing framework.

--- tests/test_rtsvg.py
+++ tests/test_rtsvg.py
@@ -1,4 +1,27 @@
+import pandas as pd
+import numpy  as np
+import polars as pl
+import unittest
+
 from rtsvg import *
 
-rt = RACETrack()
+class TestRTSVG(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rt_self = RACETrack()
+        lu = {'a':[10,   20,   12,   15,   18,   100,  101],
+              'b':['a',  'b',  'c',  'a',  'b',  'c',  'a'],
+              'c':[1,    2,    3,    1,    2,    3,    1]}
+        self.df_pd   = pd.DataFrame(lu)
+        self.df_pl   = pl.DataFrame(lu)
 
+    def test_isPandas(self):
+        self.assertTrue (self.rt_self.isPandas(self.df_pd))
+        self.assertFalse(self.rt_self.isPandas(self.df_pl))
+    
+    def test_isPolars(self):
+        self.assertFalse(self.rt_self.isPolars(self.df_pd))
+        self.assertTrue (self.rt_self.isPolars(self.df_pl))
+
+if __name__ == '__main__':
+    unittest.main()
