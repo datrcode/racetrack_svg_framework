260be2da3e653c98f32bdb9c0b196a06e3f32e7b
D Trimm <dave.trimm@gmail.com>
2024-08-21 19:48:25 -0400

    Additional work on unit tests.

--- tests/test_rt_ontologies_mixin.py
+++ tests/test_rt_ontologies_mixin.py
@@ -1,43 +1,15 @@
+import unittest
 import pandas as pd
 import polars as pl
 import numpy as np
 import json
 from rtsvg import *
-rt = RACETrack()
-def lEquals(l1, l2):
-    if len(l1) != len(l2): return False
-    for i in range(len(l1)):
-        if type(l1[i]) != type(l2[i]): return False
-        if type(l1[i]) == list:
-            l_equals = lEquals(l1[i],l2[i])
-            if l_equals == False: return False
-        if type(l1[i]) == dict:
-            d_equals = dEquals(l1[i],l2[i])
-            if d_equals == False: return False
-        if l1[i] != l2[i]: return False
-    return True
-def dEquals(d1, d2):
-    # Checks keys first
-    d1set, d2set = set(d1.keys()), set(d2.keys())
-    if len(d1set) != len(d2set): return False
-    for k in d1set: 
-        if k not in d2set: return False
 
-    # Checks values
-    for k in d1set:
-        v1, v2 = d1[k], d2[k]
-        if type(v1) != type(v2): return False
-        if type(v1) == list:
-            l_equals = lEquals(v1,v2)
-            if l_equals == False: return False
-        if type(v1) == dict:
-            d_equals = dEquals(v1,v2)
-            if d_equals == False: return False
-        if v1 != v2: return False
-    
-    return True
-
-my_json = json.loads('''
+class Testrt_ontologies_mixin(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rt_self = RACETrack()
+        self.my_json = json.loads('''
 {
   "id":      1,
   "id_str": "1",
@@ -56,25 +28,28 @@ my_json = json.loads('''
   ]
 }
 ''')
+        def test_ontologyFrameworkInstance(self):
+            self.assertTrue(jsonAbsolutePath("$.id", self.my_json) == 1)
+            self.assertTrue(jsonAbsolutePath("$.more-stuff[1].id", self.my_json) == 101)
+            self.assertTrue(jsonAbsolutePath("$.more-stuff[3].name",     self.my_json) is None)
+            self.assertTrue(jsonAbsolutePath("$.more-stuff[4].jobs[1]",  self.my_json) == 'manager')
+            self.assertTrue(jsonAbsolutePath("$.more-stuff[4].jobs[3]",  self.my_json) is None)
+            self.assertTrue(jsonAbsolutePath("$.arr_win_arr[1]",         self.my_json) == [4, 5, 6])
+            self.assertTrue(jsonAbsolutePath("$.arr_deeper[0].value",    self.my_json) == 2.3)
+            _results_ = fillJSONPathElements(["$.more-stuff[*].name"], self.my_json) 
+            self.assertDictEqual(_results_, {'$.more-stuff[*].name': ['mary', 'joe', 'fred', None, 'sally']})
+            _results_ = fillJSONPathElements(["$.more-stuff[*].name", "$.more-stuff[*].id"], self.my_json)
+            self.assertDictEqual(_results_, {'$.more-stuff[*].name': ['mary', 'joe', 'fred', None, 'sally'],'$.more-stuff[*].id': [100, 101, 102, 103, 104]})
+            _results_ = fillJSONPathElements(["$.more-stuff[*].jobs[*]", "$.more-stuff[*].id"], self.my_json) 
+            self.assertDictEqual(_results_, {'$.more-stuff[*].jobs[*]': ['scientist', 'developer', 'manager', 'accountant'], '$.more-stuff[*].id': [102, 104, 104, 104]})
+            _results_ = fillJSONPathElements(["$.arr_deeper[0].stuff[*]", "$.arr_deeper[0].value"], self.my_json)
+            self.assertDictEqual(_results_, {'$.arr_deeper[0].stuff[*]': [1, 2, 3], '$.arr_deeper[0].value': [2.3, 2.3, 2.3]})
+            _results_ = fillJSONPathElements(["$.more-stuff[*].jobs[0]", "$.more-stuff[*].id"], self.my_json)
+            self.assertDictEqual(_results_, {'$.more-stuff[*].jobs[0]': ['scientist', 'developer'], '$.more-stuff[*].id': [102, 104]})
+            _results_ = fillJSONPathElements(["$.more-stuff[*].jobs[1]", "$.more-stuff[*].id"], self.my_json)
+            self.assertDictEqual(_results_, {'$.more-stuff[*].jobs[1]': ['manager'], '$.more-stuff[*].id': [104]})
+            _results_ = fillJSONPathElements(["$.more-stuff[*].jobs[5]", "$.more-stuff[*].id"], self.my_json)
+            self.assertDictEqual(_results_, {'$.more-stuff[*].jobs[5]': [], '$.more-stuff[*].id': []})
 
-assert jsonAbsolutePath("$.id",                     my_json) == 1
-assert jsonAbsolutePath("$.more-stuff[1].id",       my_json) == 101
-assert jsonAbsolutePath("$.more-stuff[3].name",     my_json) is None
-assert jsonAbsolutePath("$.more-stuff[4].jobs[1]",  my_json) == 'manager'
-assert jsonAbsolutePath("$.more-stuff[4].jobs[3]",  my_json) is None
-assert jsonAbsolutePath("$.arr_win_arr[1]",         my_json) == [4, 5, 6]
-assert jsonAbsolutePath("$.arr_deeper[0].value",    my_json) == 2.3
-_results_ = fillJSONPathElements(["$.more-stuff[*].name"], my_json) 
-assert dEquals(_results_, {'$.more-stuff[*].name': ['mary', 'joe', 'fred', None, 'sally']})
-_results_ = fillJSONPathElements(["$.more-stuff[*].name", "$.more-stuff[*].id"], my_json)
-assert dEquals(_results_, {'$.more-stuff[*].name': ['mary', 'joe', 'fred', None, 'sally'],'$.more-stuff[*].id': [100, 101, 102, 103, 104]})
-_results_ = fillJSONPathElements(["$.more-stuff[*].jobs[*]", "$.more-stuff[*].id"], my_json) 
-assert dEquals(_results_, {'$.more-stuff[*].jobs[*]': ['scientist', 'developer', 'manager', 'accountant'], '$.more-stuff[*].id': [102, 104, 104, 104]})
-_results_ = fillJSONPathElements(["$.arr_deeper[0].stuff[*]", "$.arr_deeper[0].value"], my_json)
-assert dEquals(_results_, {'$.arr_deeper[0].stuff[*]': [1, 2, 3], '$.arr_deeper[0].value': [2.3, 2.3, 2.3]})
-_results_ = fillJSONPathElements(["$.more-stuff[*].jobs[0]", "$.more-stuff[*].id"], my_json)
-assert dEquals(_results_, {'$.more-stuff[*].jobs[0]': ['scientist', 'developer'], '$.more-stuff[*].id': [102, 104]})
-_results_ = fillJSONPathElements(["$.more-stuff[*].jobs[1]", "$.more-stuff[*].id"], my_json)
-assert dEquals(_results_, {'$.more-stuff[*].jobs[1]': ['manager'], '$.more-stuff[*].id': [104]})
-_results_ = fillJSONPathElements(["$.more-stuff[*].jobs[5]", "$.more-stuff[*].id"], my_json)
-assert dEquals(_results_, {'$.more-stuff[*].jobs[5]': [], '$.more-stuff[*].id': []})
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_rtsvg.py
+++ tests/test_rtsvg.py
@@ -25,3 +25,4 @@ class TestRTSVG(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
