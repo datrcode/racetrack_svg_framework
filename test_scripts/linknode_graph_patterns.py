32fae80388bb8d20ffd48d223d7038a036ea119a
DT <dave.trimm@gmail.com>
2025-08-03 09:06:33 -0400

    With prefix differentiation

--- test_scripts/linknode_graph_patterns.py
+++ test_scripts/linknode_graph_patterns.py
@@ -5,31 +5,31 @@ __name__ = 'linknode_graph_patterns'
 class LinkNodeGraphPatterns(object):
     def __init__(self):
         self.types = ['binarytree', 'ring', 'mesh']    
-    def createPattern(self, _type_, **kwargs):
+    def createPattern(self, _type_, prefix='', **kwargs):
         _fn_ = '__pattern_' + _type_ + '__'
-        if hasattr(self, _fn_): return getattr(self, _fn_)(**kwargs)
+        if hasattr(self, _fn_): return getattr(self, _fn_)(prefix=prefix,**kwargs)
         else:                   return None
 
-    def __pattern_binarytree__(self, depth=5, **kwargs):
+    def __pattern_binarytree__(self, depth=5, prefix='', **kwargs):
         return nx.balanced_tree(depth,2)
 
-    def __pattern_ring__(self, spokes=20, **kwargs):
+    def __pattern_ring__(self, spokes=20, prefix='', **kwargs):
         g = nx.Graph()
-        for i in range(spokes): g.add_edge(i, (i+1)%spokes)
+        for i in range(spokes): g.add_edge(prefix+str(i), prefix+str((i+1)%spokes))
         return g
 
-    def __pattern_mesh__(self, xtiles=10, ytiles=10, **kwargs):
+    def __pattern_mesh__(self, xtiles=10, ytiles=10, prefix='', **kwargs):
         g       = nx.Graph()
         _nodes_ = set()
         for _y_ in range(ytiles+1):
             for _x_ in range(xtiles+1):
-                _node_ = f'node_{_y_}_{_x_}'
+                _node_ = f'{prefix}node_{_y_}_{_x_}'
                 _nodes_.add(_node_)
         for _node_ in _nodes_:
-            _y_, _x_ = int(_node_.split('_')[1]), int(_node_.split('_')[2])
+            _y_, _x_ = int(_node_.split('_')[-1]), int(_node_.split('_')[-2])
             for dy in [-1, 0, 1]:
                 for dx in [-1, 0, 1]:
                     if (dx == 0 and dy == 0) or (abs(dx) == 1 and abs(dy) == 1): continue
-                    _nbor_ = f'node_{_y_+dy}_{_x_+dx}'
+                    _nbor_ = f'{prefix}node_{_y_+dy}_{_x_+dx}'
                     if _nbor_ in _nodes_: g.add_edge(_node_, _nbor_)
         return g
