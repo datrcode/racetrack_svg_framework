715ae140a87bb06b6b8e2a234405746e0e9f3793
DT <dave.trimm@gmail.com>
2025-08-05 22:52:26 -0400

    Demonstration of weighted graph spring layouts.

--- test_scripts/linknode_graph_patterns.py
+++ test_scripts/linknode_graph_patterns.py
@@ -1,14 +1,19 @@
 import networkx as nx
+import re
 
 __name__ = 'linknode_graph_patterns'
 
 class LinkNodeGraphPatterns(object):
     def __init__(self):
-        self.types = ['binarytree', 'ring', 'mesh']    
+        self.types = []
+        for _str_ in dir(self):
+            _match_ = re.match('__pattern_(.*)__', _str_)
+            if _match_ is not None: self.types.append(_match_.group(1))
+
     def createPattern(self, _type_, prefix='', **kwargs):
+        if _type_ not in self.types: raise Exception(f'Unknown pattern type: {_type_}')
         _fn_ = '__pattern_' + _type_ + '__'
-        if hasattr(self, _fn_): return getattr(self, _fn_)(prefix=prefix,**kwargs)
-        else:                   return None
+        return getattr(self, _fn_)(prefix=prefix,**kwargs)
 
     def __pattern_binarytree__(self, depth=5, prefix='', **kwargs):
         return nx.balanced_tree(depth,2)
@@ -33,3 +38,32 @@ class LinkNodeGraphPatterns(object):
                     _nbor_ = f'{prefix}node_{_y_+dy}_{_x_+dx}'
                     if _nbor_ in _nodes_: g.add_edge(_node_, _nbor_)
         return g
+
+    def __pattern_boxinbox__(self, **kwargs):
+        pos = {'ul': (0.0, 0.0), 'um': (0.5, 0.0), 'ur': (1.0, 0.0), 
+               'ml': (0.0, 0.5),                   'mr': (1.0, 0.5),
+               'll': (0.0, 1.0), 'lm': (0.5, 1.0), 'lr': (1.0, 1.0),
+               'inner_ul': (0.1, 0.1),             'inner_ur': (0.9, 0.1),
+               'inner_ll': (0.1, 0.9),             'inner_lr': (0.9, 0.9)}
+
+        def d(a, b): return 1.0 / (((pos[a][0]-pos[b][0])**2 + (pos[a][1]-pos[b][1])**2)**0.5)
+
+        g   = nx.Graph()
+        g.add_edge('ul', 'um', weight=d('ul', 'um')), g.add_edge('um', 'ur', weight=d('um', 'ur'))
+        g.add_edge('ul', 'ml', weight=d('ul', 'ml')), g.add_edge('ml', 'll', weight=d('ml', 'll'))
+        g.add_edge('ur', 'mr', weight=d('ur', 'mr')), g.add_edge('mr', 'lr', weight=d('mr', 'lr'))
+        g.add_edge('ll', 'lm', weight=d('ll', 'lm')), g.add_edge('lm', 'lr', weight=d('lm', 'lr'))
+
+        g.add_edge('ul', 'inner_ul', weight=d('ul', 'inner_ul'))
+        g.add_edge('ur', 'inner_ur', weight=d('ur', 'inner_ur'))
+        g.add_edge('lr', 'inner_lr', weight=d('lr', 'inner_lr'))
+        g.add_edge('ll', 'inner_ll', weight=d('ll', 'inner_ll'))
+
+        g.add_edge('inner_ul', 'inner_ur', weight=d('inner_ul', 'inner_ur'))
+        g.add_edge('inner_ur', 'inner_lr', weight=d('inner_ur', 'inner_lr'))
+        g.add_edge('inner_lr', 'inner_ll', weight=d('inner_lr', 'inner_ll'))
+        g.add_edge('inner_ll', 'inner_ul', weight=d('inner_ll', 'inner_ul'))
+
+        return g
+
+
