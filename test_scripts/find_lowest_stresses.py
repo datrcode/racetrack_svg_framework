0235630919880a4794541f42f91d780cd79d79cf
DT <dave.trimm@gmail.com>
2025-10-07 10:39:34 -0400

    Ensured distances only calculated once

--- test_scripts/find_lowest_stresses.py
+++ test_scripts/find_lowest_stresses.py
@@ -1,5 +1,6 @@
 import polars as pl
 import rtsvg
+rt = rtsvg.RACETrack()
 import time
 from linknode_graph_patterns import LinkNodeGraphPatterns
 from convey_proximity_layout import ConveyProximityLayout
@@ -13,14 +14,15 @@ _patterns_ = LinkNodeGraphPatterns()
 for i in range(len(_patterns_)):
     _pattern_ = _patterns_[i]
     print(_pattern_,end=' ')
-    _g_       = _patterns_.createPattern(_pattern_)
+    _g_         = _patterns_.createPattern(_pattern_)
+    _distances_ = rt.distanceDictionary(_g_)
     _stress_min_, _algo_min_, _algo_min_str_ = None, None, None
     for k in range(3):
-        for _trial_ in range(200):
+        for _trial_ in range(3):
             for _algo_no_ in range(2):
                 t0 = time.time()
-                if _algo_no_ == 0: _algo_, _algo_str_  = rtsvg.PolarsForceDirectedLayout(_g_, k=k),                      'PFDL'
-                else:              _algo_, _algo_str_  = ConveyProximityLayout(_g_, k=k, use_resistive_distances=False), 'CPL'
+                if _algo_no_ == 0: _algo_, _algo_str_  = rtsvg.PolarsForceDirectedLayout(_g_, k=k, distances=_distances_), 'PFDL'
+                else:              _algo_, _algo_str_  = ConveyProximityLayout          (_g_, k=k, distances=_distances_), 'CPL'
                 t1 = time.time()
                 _pos_    = _algo_.results()
                 _stress_ = _algo_.stress()
